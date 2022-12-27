502abdd514602decb15f41811dcbcbe6ca223518
woni <jessica901215@gmail.com>
2022-12-27 10:08:46 +0800

    feat: 新增CompanyEditComponent、CompanyAddComponent、BillDetailComponent頁面 & 路徑

--- src/app/views/container/container-routing.module.ts
+++ src/app/views/container/container-routing.module.ts
@@ -3,7 +3,10 @@ import { ContainerComponent } from './container.component';
 import { RouterModule, Routes } from '@angular/router';
 import { OrderComponent } from '../features/order/order.component';
 import { BillComponent } from '../features/bill/bill.component';
+import { BillDetailComponent } from '../features/bill/bill-detail/bill-detail.component';
 import { CompanyComponent } from '../features/company/company.component';
+import { CompanyAddComponent } from '../features/company/company-add/company-add.component';
+import { CompanyEditComponent } from '../features/company/company-edit/company-edit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
@@ -13,7 +16,10 @@ const routes: Routes = [
       { path: 'dashboard', loadChildren: () => import('../features/dashboard/dashboard.module').then(m => m.DashboardModule) },
       { path: 'order', component: OrderComponent },
       { path: 'bill', component: BillComponent },
+      { path: 'bill/bill-detail', component: BillDetailComponent },
       { path: 'company', component: CompanyComponent },
+      { path: 'company/add-company', component: CompanyAddComponent },
+      { path: 'company/edit-company', component: CompanyEditComponent },
     ],
   },
 ];
