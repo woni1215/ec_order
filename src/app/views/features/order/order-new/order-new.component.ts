b6ec21d223d3f40ec1b84115a887f50101950b29
abcdefuuuu <sacxs1234621@gmail.com>
2022-12-27 16:00:27 +0800

    feat:訂單管理編輯、新增UI

--- src/app/views/features/order/order-new/order-new.component.ts
+++ src/app/views/features/order/order-new/order-new.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 interface option {
+  name: string,
+}
+interface option2 {
   name: string,
 }
 @Component({
@@ -9,12 +13,22 @@ interface option {
   styleUrls: ['./order-new.component.scss']
 })
 export class OrderNewComponent {
-  op: { name: string}[];
-  constructor() {
+  op: option[];
+  op2: option2[];
+  constructor(private messageService: MessageService,) {
     this.op = [
-        {name: '商品A'},
-        {name: '商品B'},
-        {name: '商品C'},
+        {name: '廠商A'},
+        {name: '廠商B'},
+        {name: '廠商C'},
     ];
+    this.op2 = [
+      {name: '商品A'},
+      {name: '商品B'},
+      {name: '商品C'},
+  ];
+}
+showSuccess() {
+  this.messageService.add({severity:'success', summary: 'Success', detail: '新增成功，1秒後跳轉至訂單列表頁面'});
+  setTimeout("location.href='/main/order'",1000); // 1秒後跳轉頁面
 }
 }
