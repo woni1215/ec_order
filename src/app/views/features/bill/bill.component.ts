7f3978e597991a056eaf307151bd0491aa5d4cdd
woni <jessica901215@gmail.com>
2023-01-03 09:42:48 +0800

    feat: 結算單匯出excel

--- src/app/views/features/bill/bill.component.ts
+++ src/app/views/features/bill/bill.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpApiService } from 'src/app/api/http-api.service';
 
 @Component({
   selector: 'app-bill',
@@ -13,7 +14,26 @@ export class BillComponent {
       status: '已送出',
     }
   ]
-  export(){
-    window.location.assign('main/bill/bill-detail')
+  GETAllOrder: HttpApiService[] = [];
+  constructor(
+    private HttpApi: HttpApiService,
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllOrderRequest();
+  }
+
+  o_id: any
+  // 切換到結算頁面
+  detail(o_id: any) {
+    window.location.assign('main/bill/bill-detail/' + o_id)
+  }
+
+  // 取得結算單
+  getAllOrderRequest() {
+    this.HttpApi.getAllOrderRequest(1).subscribe(res => {
+      this.GETAllOrder = res.body
+      console.log(res.body)
+    })
   }
 }
