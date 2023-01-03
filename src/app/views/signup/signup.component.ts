217f5c61801d5b5adab33d3a51e9b0f4183b2dd5
abcdefuuuu <sacxs1234621@gmail.com>
2023-01-03 18:45:52 +0800

    feat: account

--- src/app/views/signup/signup.component.ts
+++ src/app/views/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MessageService } from 'primeng/api';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { HttpApiService } from './../../api/http-api.service';
 
 @Component({
   selector: 'app-signup',
@@ -9,19 +11,64 @@ import { MessageService } from 'primeng/api';
 
 })
 export class SignupComponent {
+  POSTNewAccount: HttpApiService[] = [];
+  signupdata: any = [{
+    name: [''],
+    acccount: [''],
+    pwd: [''],
+    s_id: [''],
+    role_id: [''],
+  }]
+  FormGroup: FormGroup;
   constructor(
-    private messageService: MessageService,
-) { }
+    private HttpApi: HttpApiService, private messageService: MessageService, private fb: FormBuilder
+  ) {
+    this.FormGroup = this.fb.group({
+      name: [''],
+      acccount: [''],
+      pwd: [''],
+      s_id: [''],
+      role_id: [''],
+    });
+  }
 
-ngOnInit() {
-}
-showSuccess() {
-  this.messageService.add({severity:'success', summary: 'Success', detail: '成功註冊，1秒後導回登入頁面!'});
-  setTimeout("location.href='/login'",1000); // 1秒後跳轉頁面
-}
-showError() {
-  this.messageService.add({severity:'error', summary: 'Error', detail: '註冊失敗，1秒導回登入頁面!'});
-  setTimeout("location.href='/login'",1000); // 1秒後跳轉頁面
-}
+  ngOnInit(): void {
+    this.FormGroup = this.fb.group({
+      name: new FormControl(),
+      acccount: new FormControl(),
+      pwd: new FormControl(),
+      role_id: new FormControl(),
+    })
+  }
+  postAccountRequest() {
+    let body = {
+      name: this.signupdata.name,
+      acccount: this.signupdata.acccount,
+      pwd: this.signupdata.pwd,
+      role_id: 1,
+    }
+    this.HttpApi.postProductRequest(body)
+      .subscribe(Request => {
+        console.log(body)
+        this.POSTNewAccount = [Request]
+        console.log(Request)
+        this.signupdata.s_id = Request.body
+        console.log("取到的s_id: " + this.signupdata.s_id)
+      },
+        error => {
+          console.log(error);
+        })
+  }
+  //RadioButton
+  optiondialog: any[] = [{ name: '一般使用者　', key: 'option1' }, { name: '管理者　', key: 'option2' }];
+
+  showSuccess() {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: '成功註冊，1秒後導回登入頁面!' });
+    setTimeout("location.href='/login'", 1000); // 1秒後跳轉頁面
+  }
+  showError() {
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: '註冊失敗，1秒導回登入頁面!' });
+    setTimeout("location.href='/login'", 1000); // 1秒後跳轉頁面
+  }
 
 }
